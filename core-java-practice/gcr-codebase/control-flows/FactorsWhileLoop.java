424011a713311b21988ba8805f37ddfcdcade700
Ayush Gupta <ag6691602@gmail.com>
2025-12-19 16:51:52 +0530

    [Ayush Gupta] Add. Added Level 2 Questions of Control Flows

---
+++ core-java-practice/gcr-codebase/control-flows/FactorsWhileLoop.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FactorsWhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive number: ");
+        int number = sc.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Not a positive number.");
+        } else {
+            System.out.println("Factors of " + number + " are:");
+            int counter = 1;
+            while (counter <= number) {
+                if (number % counter == 0) {
+                    System.out.println(counter);
+                }
+                counter++;
+            }
+        }
+        sc.close();
+    }
+}
