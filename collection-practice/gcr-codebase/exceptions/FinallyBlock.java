49482f73751852b7d5363d05f2284973dae7c23e
Ayush Gupta <ag6691602@gmail.com>
2026-01-09 10:46:51 +0530

    [Ayush gupta] Add. Added Questions For Java Exceptions.

---
+++ collection-practice/gcr-codebase/exceptions/FinallyBlock.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class FinallyBlock {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero");
+        } finally {
+            System.out.println("Operation completed");
+        }
+        sc.close();
+    }
+}
